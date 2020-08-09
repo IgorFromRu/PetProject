c21f1afcd747782586212fef4023cda4c7632a6b
TimurMuratov <yeicauM2ua4c>
2020-08-09 11:26:26 +0400

    PRO-2. Tests

--- src/main/java/com/example/homecompany/petproject/controller/UserController.java
+++ src/main/java/com/example/homecompany/petproject/controller/UserController.java
@@ -7,9 +7,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 
-@RestController("users")
+@RestController
+@RequestMapping("users")
 public class UserController {
 
     @Autowired
